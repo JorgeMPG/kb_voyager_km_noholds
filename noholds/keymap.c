fcead6e013e7252eef95c124fd93ea3333e6e75d
Jorge MPG <22392268+JorgeMPG@users.noreply.github.com>
2024-04-12 17:39:04 +0200

    chore: Name layers using an enum

--- noholds/keymap.c
+++ noholds/keymap.c
@@ -37,8 +37,17 @@ enum tap_dance_codes {
   DANCE_0,
 };
 
+// Layer Declarations
+enum {
+    _BASE,
+    _L_OSL,
+    _L_NAV,
+    _L_NUM,
+    // ..., the rest of your layers
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [0] = LAYOUT_voyager(
+  [_BASE] = LAYOUT_voyager(
     //------------- ,  -------------- ,  -------------- ,  -------------- ,  -------------- ,  ---------SPLIT ,  SPLIT--------- ,  -------------- ,  -------------- ,  -------------- ,  -------------- ,  --------------,
     KC_ESCAPE       ,  KC_1           ,  KC_2           ,  KC_3           ,  KC_4           ,  KC_5           ,  KC_6           ,  KC_7           ,  KC_8           ,  KC_9           ,  KC_0           ,  KC_BACKSPACE,
     KC_TAB          ,  KC_Q           ,  KC_W           ,  KC_E           ,  KC_R           ,  KC_T           ,  KC_Y           ,  KC_U           ,  KC_I           ,  KC_O           ,  KC_P           ,  KC_MINUS,
@@ -46,9 +55,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     OSM(MOD_LCTL)   ,  KC_Z           ,  KC_X           ,  KC_C           ,  KC_V           ,  KC_B           ,  KC_N           ,  KC_M           ,  KC_COMMA       ,  KC_DOT         ,  KC_SLASH       ,  KC_ENTER,
     // Thumb cluster
     //-------------- ,  ---------SPLIT ,  SPLIT--------- ,  --------------
-      OSL(1)         ,  KC_SPACE       ,  KC_SPACE       ,  OSL(1)
+      OSL(_L_OSL)    ,  KC_SPACE       ,  KC_SPACE       ,  OSL(_L_OSL)
   ),
-  [1] = LAYOUT_voyager(
+  [_L_OSL] = LAYOUT_voyager(
     //------------- ,  -------------- ,  -------------- ,  -------------- ,  -------------- ,  ---------SPLIT ,  SPLIT--------- ,  -------------- ,  -------------- ,  -------------- ,  -------------- ,  --------------,
     KC_ESCAPE       ,  KC_F1          ,  KC_F2          ,  KC_F3          ,  KC_F4          ,  KC_F5          ,  KC_F6          ,  KC_F7          ,  KC_F8          ,  KC_F9          ,  KC_F10         ,  KC_F11,
     KC_GRAVE        ,  KC_EXLM        ,  KC_AT          ,  KC_HASH        ,  KC_DLR         ,  KC_PERC        ,  KC_7           ,  KC_8           ,  KC_9           ,  KC_MINUS       ,  KC_SLASH       ,  KC_F12,
@@ -58,7 +67,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //-------------- ,  ---------SPLIT ,  SPLIT--------- ,  --------------
       KC_TRANSPARENT ,  KC_TRANSPARENT ,  KC_TRANSPARENT ,  KC_0
   ),
-  [2] = LAYOUT_voyager(
+  [_L_NAV] = LAYOUT_voyager(
     //------------- ,  --------------                                        ,  --------------                                  ,  -------------- ,  -------------- ,  ---------SPLIT  ,  SPLIT--------- ,  --------------                  ,  -------------- ,  -------------- ,  -------------- ,  --------------,
     RGB_TOG         ,  TOGGLE_LAYER_COLOR,RGB_MOD                            ,  RGB_SLD                                         ,  RGB_VAD        ,  RGB_VAI        ,  KC_TRANSPARENT  ,  KC_TRANSPARENT ,  KC_TRANSPARENT                  ,  KC_TRANSPARENT ,  KC_TRANSPARENT ,  KC_TRANSPARENT,
     KC_TRANSPARENT  ,  KC_TRANSPARENT                                        ,  KC_AUDIO_VOL_DOWN,KC_AUDIO_VOL_UP,KC_AUDIO_MUTE ,  KC_TRANSPARENT ,  KC_PGUP        ,  KC_HOME         ,  KC_UP          ,  KC_END                          ,  KC_TRANSPARENT ,  KC_TRANSPARENT,
