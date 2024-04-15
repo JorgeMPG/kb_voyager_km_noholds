35f77022a94cc69f96f038fde5d4781677c1ba77
Jorge MPG <22392268+JorgeMPG@users.noreply.github.com>
2024-04-15 18:19:50 +0200

    feat: Disable OSL upon OSM tap.
    Restore deprecated functionality of OSL layers so the layer returns to
    base upon tapping an OSM.

--- noholds/keymap.c
+++ noholds/keymap.c
@@ -117,6 +117,16 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   return true;
 }
 
+// https://github.com/qmk/qmk_firmware/issues/22566
+// Restore deprecated OSL quirk: Disable OSL upon OSM tap.
+void post_process_record_user(uint16_t keycode, keyrecord_t *record) {
+    if (IS_QK_ONE_SHOT_MOD(keycode) && is_oneshot_layer_active() && record->event.pressed) {
+        clear_oneshot_layer_state(ONESHOT_OTHER_KEY_PRESSED);
+    }
+    return;
+}
+
+
 
 typedef struct {
     bool is_press_action;
