c19f860a35d3514c07166f6260277294d50419c4
Jorge MPG <22392268+JorgeMPG@users.noreply.github.com>
2024-04-15 18:19:07 +0200

    Populate left OSL layer

--- noholds/keymap.c
+++ noholds/keymap.c
@@ -49,20 +49,20 @@ enum {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_voyager(
     //------------- ,  -------------- ,  -------------- ,  -------------- ,  -------------- ,  ---------SPLIT ,  SPLIT--------- ,  -------------- ,  -------------- ,  -------------- ,  -------------- ,  --------------,
-    KC_ESCAPE       ,  KC_1           ,  KC_2           ,  KC_3           ,  KC_4           ,  KC_5           ,  KC_6           ,  KC_7           ,  KC_8           ,  KC_9           ,  KC_0           ,  KC_BACKSPACE,
-    KC_TAB          ,  KC_Q           ,  KC_W           ,  KC_E           ,  KC_R           ,  KC_T           ,  KC_Y           ,  KC_U           ,  KC_I           ,  KC_O           ,  KC_P           ,  KC_MINUS,
-    OSM(MOD_LSFT)   ,  KC_A           ,  KC_S           ,  KC_D           ,  KC_F           ,  KC_G           ,  KC_H           ,  KC_J           ,  KC_K           ,  KC_L           ,  KC_SCLN        ,  KC_QUOTE,
-    OSM(MOD_LCTL)   ,  KC_Z           ,  KC_X           ,  KC_C           ,  KC_V           ,  KC_B           ,  KC_N           ,  KC_M           ,  KC_COMMA       ,  KC_DOT         ,  KC_SLASH       ,  KC_ENTER,
+    KC_ESCAPE       ,  KC_1           ,  KC_2           ,  KC_3           ,  KC_4           ,  KC_5           ,  KC_6           ,  KC_7           ,  KC_8           ,  KC_9           ,  KC_0           ,  KC_BACKSPACE ,
+    KC_TAB          ,  KC_Q           ,  KC_W           ,  KC_E           ,  KC_R           ,  KC_T           ,  KC_Y           ,  KC_U           ,  KC_I           ,  KC_O           ,  KC_P           ,  KC_MINUS ,
+    OSM(MOD_LSFT)   ,  KC_A           ,  KC_S           ,  KC_D           ,  KC_F           ,  KC_G           ,  KC_H           ,  KC_J           ,  KC_K           ,  KC_L           ,  KC_SCLN        ,  KC_QUOTE ,
+    OSM(MOD_LCTL)   ,  KC_Z           ,  KC_X           ,  KC_C           ,  KC_V           ,  KC_B           ,  KC_N           ,  KC_M           ,  KC_COMMA       ,  KC_DOT         ,  KC_SLASH       ,  KC_ENTER ,
     // Thumb cluster
     //-------------- ,  ---------SPLIT ,  SPLIT--------- ,  --------------
       OSL(_L_OSL)    ,  KC_SPACE       ,  KC_SPACE       ,  OSL(_L_OSL)
   ),
   [_L_OSL] = LAYOUT_voyager(
     //------------- ,  -------------- ,  -------------- ,  -------------- ,  -------------- ,  ---------SPLIT ,  SPLIT--------- ,  -------------- ,  -------------- ,  -------------- ,  -------------- ,  --------------,
-    KC_ESCAPE       ,  KC_F1          ,  KC_F2          ,  KC_F3          ,  KC_F4          ,  KC_F5          ,  KC_F6          ,  KC_F7          ,  KC_F8          ,  KC_F9          ,  KC_F10         ,  KC_F11,
-    KC_GRAVE        ,  KC_EXLM        ,  KC_AT          ,  KC_HASH        ,  KC_DLR         ,  KC_PERC        ,  KC_7           ,  KC_8           ,  KC_9           ,  KC_MINUS       ,  KC_SLASH       ,  KC_F12,
-    KC_TRANSPARENT  ,  KC_CIRC        ,  KC_AMPR        ,  KC_ASTR        ,  KC_LPRN        ,  KC_RPRN        ,  KC_4           ,  KC_5           ,  KC_6           ,  KC_PLUS        ,  KC_ASTR        ,  KC_BSPC,
-    KC_TRANSPARENT  ,  KC_TRANSPARENT ,  KC_LBRC        ,  KC_RBRC        ,  KC_LCBR        ,  KC_RCBR        ,  KC_1           ,  KC_2           ,  KC_3           ,  KC_DOT         ,  KC_EQUAL       ,  KC_ENTER,
+    KC_F11          ,  KC_F1          ,  KC_F2          ,  KC_F3          ,  KC_F4          ,  KC_F5          ,  KC_F6          ,  KC_F7          ,  KC_F8          ,  KC_F9          ,  KC_F10         ,  KC_F11 ,
+    KC_F12          ,  KC_EXLM        ,  KC_TAB         ,  OSM(MOD_LCTL)  ,  OSM(MOD_LALT)  ,  KC_BSPC        ,  _______        ,  _______        ,  _______        ,  _______        ,  _______        ,  KC_F12 ,
+    _______         ,  KC_CIRC        ,  KC_ENTER       ,  OSM(MOD_LGUI)  ,  OSM(MOD_LSFT)  ,  KC_ESCAPE      ,  _______        ,  _______        ,  _______        ,  _______        ,  _______        ,  _______ ,
+    _______         ,  _______        ,  TO(_L_NUM)     ,  TO(_L_NAV)     ,  KC_BACKSLASH   ,  KC_DEL         ,  _______        ,  _______        ,  _______        ,  _______        ,  _______        ,  _______ ,
     // Thumb cluster
     //-------------- ,  ---------SPLIT ,  SPLIT--------- ,  --------------
       KC_TRANSPARENT ,  KC_TRANSPARENT ,  KC_TRANSPARENT ,  KC_0
