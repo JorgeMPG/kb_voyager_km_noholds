4508f41fc6381d0dece0a70bcef257b3fabea23f
Jorge MPG <22392268+JorgeMPG@users.noreply.github.com>
2024-04-18 09:43:43 +0200

    Include left num layer

--- noholds/keymap.c
+++ noholds/keymap.c
@@ -65,7 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______         ,  _______        ,  TO(_L_NUM)     ,  TO(_L_NAV)     ,  KC_BACKSLASH   ,  KC_DEL         ,  _______        ,  _______        ,  _______        ,  _______        ,  _______        ,  _______ ,
     // Thumb cluster
     //-------------- ,  ---------SPLIT ,  SPLIT--------- ,  --------------
-      KC_TRANSPARENT ,  KC_TRANSPARENT ,  KC_TRANSPARENT ,  KC_0
+      XXXXXXX        ,  XXXXXXX        ,  XXXXXXX        ,  XXXXXXX
   ),
   [_L_NAV] = LAYOUT_voyager(
     //------------- ,  --------------                                        ,  --------------                                  ,  -------------- ,  -------------- ,  ---------SPLIT  ,  SPLIT--------- ,  --------------                  ,  -------------- ,  -------------- ,  -------------- ,  --------------,
@@ -75,8 +75,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TRANSPARENT  ,  KC_TRANSPARENT                                        ,  KC_TRANSPARENT                                  ,  HSV_0_255_255  ,  HSV_74_255_255 ,  HSV_169_255_255 ,  KC_TRANSPARENT ,  LCTL(LSFT(KC_TAB)),LCTL(KC_TAB) ,  KC_TRANSPARENT ,  KC_TRANSPARENT ,  KC_TRANSPARENT,
     // Thumb cluster
     //-------------- ,  ---------SPLIT ,  SPLIT--------- ,  --------------
-      KC_TRANSPARENT ,  KC_TRANSPARENT ,  KC_TRANSPARENT ,  KC_TRANSPARENT
+      XXXXXXX        ,  TO(_BASE)      ,  TO(_BASE)      ,  XXXXXXX
   ),
+  [_L_NUM] = LAYOUT_voyager(
+    //------------- ,  -------------- ,  -------------- ,  -------------- ,  -------------- ,  ---------SPLIT ,  SPLIT--------- ,  -------------- ,  -------------- ,  -------------- ,  -------------- ,  --------------,
+    KC_ESCAPE       ,  KC_1           ,  KC_2           ,  KC_3           ,  KC_4           ,  KC_5           ,  KC_6           ,  KC_7           ,  KC_8           ,  KC_9           ,  KC_0           ,  KC_BACKSPACE ,
+    KC_TAB          ,  KC_6           ,  KC_7           ,  KC_8           ,  KC_9           ,  KC_0           ,  KC_Y           ,  KC_U           ,  KC_I           ,  KC_O           ,  KC_P           ,  KC_MINUS ,
+    OSM(MOD_LSFT)   ,  KC_A           ,  KC_S           ,  KC_D           ,  KC_F           ,  KC_G           ,  KC_H           ,  KC_J           ,  KC_K           ,  KC_L           ,  KC_SCLN        ,  KC_QUOTE ,
+    OSM(MOD_LCTL)   ,  KC_Z           ,  KC_X           ,  KC_C           ,  KC_V           ,  KC_B           ,  KC_N           ,  KC_M           ,  KC_COMMA       ,  KC_DOT         ,  KC_SLASH       ,  KC_ENTER ,
+    // Thumb cluster
+    //-------------- ,  ---------SPLIT ,  SPLIT--------- ,  --------------
+      XXXXXXX        ,  TO(_BASE)      ,  TO(_BASE)      ,  XXXXXXX
+  )
 };
 
 const uint16_t PROGMEM combo0[] = { MT(MOD_RSFT, KC_QUOTE), MT(MOD_LSFT, KC_BSPC), COMBO_END};
